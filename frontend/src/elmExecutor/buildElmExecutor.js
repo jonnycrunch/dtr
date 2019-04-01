deee9473d60404fe589ed57547eb7700665f8297
Cole Springate-Combs <cspringatecombs@mitre.org>
2019-04-01 11:53:01 -0400

    cql executing against fhir server, working

--- frontend/src/elmExecutor/buildElmExecutor.js
+++ frontend/src/elmExecutor/buildElmExecutor.js
@@ -4,40 +4,37 @@ import fhirhelpersElm from "./FHIRHelpers.json";
 import extractFhirResourcesThatNeedFetching from "./extractFhirResourcesThatNeedFetching";
 import buildPopulatedResourceBundle from "./buildPopulatedResourceBundle";
 
-// use result like r(elm, valueSetDB, resolve, reject)
-function buildElmExecutor(smart, fhirVersion) {
-  if (fhirVersion == "dstu2") {
-    const patientSource = cqlfhir.PatientSource.FHIRv102();
-    return collectResourcesAndExecuteElm.bind(null, smart, patientSource);
-  }
-}
-
-function collectResourcesAndExecuteElm(smart, patientSource, elm, valueSetDB, resolve, reject) {
-  const neededResources = extractFhirResourcesThatNeedFetching(elm);
+function executeElm(smart, fhirVersion, executionInputs, resolve, reject) {
+  const patientSource = getPatientSource(fhirVersion)
+  const neededResources = extractFhirResourcesThatNeedFetching(executionInputs.elm);
   console.log("We need to fetch these resources:", neededResources);
   buildPopulatedResourceBundle(
     smart,
     neededResources,
     function(resourceBundle) {
       console.log("Fetched resources are in this bundle:", resourceBundle);
-      const results = executeELM(elm, resourceBundle, valueSetDB, patientSource);
+      patientSource.loadBundles([resourceBundle]);
+      const results = executeElmAgainstPatientSource(executionInputs, patientSource);
       resolve(results);
     },
     reject
   );
 }
 
-function executeELM(elm, resourceBundle, valueSetDB, patientSource) {
-  const elmDependencies = {
-    FHIRHelpers: fhirhelpersElm
-  };
-  const repository = new cql.Repository(elmDependencies);
-  const lib = new cql.Library(elm, repository);
-  const codeService = new cql.CodeService(valueSetDB);
-  const executor = new cql.Executor(lib, codeService);
-  patientSource.loadBundles([resourceBundle]);
+function executeElmAgainstPatientSource(executionInputs, patientSource) {
+  executionInputs.elmDependencies = [ fhirhelpersElm ]
+  const repository = new cql.Repository(executionInputs.elmDependencies);
+  const lib = new cql.Library(executionInputs.elm, repository);
+  const codeService = new cql.CodeService(executionInputs.valueSetDB);
+  const executor = new cql.Executor(lib, codeService, executionInputs.parameters);
   const results = executor.exec(patientSource);
   return results.patientResults[Object.keys(results.patientResults)[0]];
 }
 
-export default buildElmExecutor;
+function getPatientSource(fhirVersion) {
+  if (fhirVersion == "dstu2") return cqlfhir.PatientSource.FHIRv102();
+  if (fhirVersion == "stu3") return cqlfhir.PatientSource.FHIRv300();
+}
+
+
+export default executeElm;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,17 @@
 import "whatwg-fetch"; // a window.fetch polyfill
 import "fhirclient"; // sets window.FHIR
-import demoElm from "./cql/Demo.json";
-import buildElmExecutor from "./elmExecutor/buildElmExecutor";
+// import demoElm from "./cql/Demo.json";
+import cqlfhir from "cql-exec-fhir";
+import executeElm from "./elmExecutor/buildElmExecutor";
 import urlUtils from "./util/url";
 
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
 
-// const valueSetDB = {};
+const valueSetDB = {};
 
 // // get the URL parameters received from the authorization server
 // const state = urlUtils.getUrlParameter("state"); // session key
@@ -37,46 +38,109 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // };
 // if (secret) headers["Authorization"] = "Basic " + btoa(clientId + ":" + secret);
 
-// // obtain authorization token from the authorization service using the authorization code
-// // fetch(tokenUri, {
-// //   method: "POST",
-// //   headers: headers,
-// //   body: data.toString()
-// // })
-// //   .then(res => res.json())
-// //   // should get back the access token and maybe the patient ID
-// //   .then(function (auth_response) {
-// //     const patientId = patientIdfromAppParams || auth_response.patient;
-// //     if (patientId == null) {
-// //       errorMsg = "Failed to get a patientId from the app params or the authorization response.";
-// //       document.body.innerText = errorMsg;
-// //       console.error(errorMsg);
-// //       return;
-// //     }
-// //     var smart = FHIR.client({
-// //       serviceUrl: serviceUri,
-// //       patientId: patientId,
-// //       auth: {
-// //         type: "bearer",
-// //         token: auth_response.access_token
-// //       }
-// //     });
-// //     go(smart);
-// //   });
+// obtain authorization token from the authorization service using the authorization code
+// fetch(tokenUri, {
+//   method: "POST",
+//   headers: headers,
+//   body: data.toString()
+// })
+//   .then(res => res.json())
+//   // should get back the access token and maybe the patient ID
+//   .then(function (auth_response) {
+//     const patientId = patientIdfromAppParams || auth_response.patient;
+//     if (patientId == null) {
+//       errorMsg = "Failed to get a patientId from the app params or the authorization response.";
+//       document.body.innerText = errorMsg;
+//       console.error(errorMsg);
+//       return;
+//     }
+//     var smart = FHIR.client({
+//       serviceUrl: serviceUri,
+//       patientId: patientId,
+//       auth: {
+//         type: "bearer",
+//         token: auth_response.access_token
+//       }
+//     });
+//     go(smart);
+//   });
 
-// function go(smart) {
-//   const elmExecutor = buildElmExecutor(smart, "dstu2");
-//   // elmExecutor(
-//   //   demoElm,
-//   //   valueSetDB,
-//   //   function(results) {
-//   //     console.log("RESULTS", results);
-//   //     document.body.innerHTML = "RESULTS<pre>" + JSON.stringify(results, null, 2) + "</pre>";
-//   //   },
-//   //   function(error) {
-//   //     console.error("ERROR", error);
-//   //     document.body.innerHTML = "ERROR<pre>" + JSON.stringify(error, null, 2) + "</pre>";
-//   //   }
-//   // );
-//   //ReactDOM.render(<App />, document.getElementById("root"));
-// }
+
+const devreq = {
+  "resourceType": "DeviceRequest",
+  "id": "devreq013",
+  "meta": {
+    "versionId": "1",
+    "lastUpdated": "2019-03-28T12:04:26.239-04:00",
+    "profile": [
+      "http://hl7.org/fhir/us/davinci-crd/STU3/StructureDefinition/profile-devicerequest-stu3"
+    ]
+  },
+  "extension": [
+    {
+      "url": "http://build.fhir.org/ig/HL7/davinci-crd/STU3/ext-insurance.html",
+      "valueReference": {
+        "reference": "Coverage/cov013"
+      }
+    }
+  ],
+  "status": "draft",
+  "codeCodeableConcept": {
+    "coding": [
+      {
+        "system": "https://bluebutton.cms.gov/resources/codesystem/hcpcs",
+        "code": "E0424",
+        "display": "Stationary Compressed Gaseous Oxygen System, Rental"
+      }
+    ]
+  },
+  "subject": {
+    "reference": "Patient/pat013"
+  },
+  "performer": {
+    "reference": "Practitioner/pra1234"
+  }
+}
+
+const fhirWrapper = cqlfhir.FHIRWrapper.FHIRv300()
+const fhir_devreq = fhirWrapper.wrap(devreq)
+
+var smart = FHIR.client({
+  serviceUrl: "http://localhost:8080/fhir",
+  patientId: "pat013"
+});
+
+var old_elm_string = "";
+
+function go() {
+  fetch('Demo.json')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(elmJson) {
+      let elm_string = JSON.stringify(elmJson)
+      if (old_elm_string == elm_string) return;
+      old_elm_string = elm_string;
+      document.body.innerHTML = "Calculating...";
+      const executionInputs = {
+        elm: elmJson,
+        elmDependencies: {},
+        valueSetDB: valueSetDB,
+        parameters: {device_request: fhir_devreq}
+        // parameters: {}
+      }
+      executeElm(smart, "stu3", executionInputs,
+        function(results) {
+          console.log("RESULTS", results);
+          document.body.innerHTML = "Done, results in console.";
+          // document.body.innerHTML = "RESULTS<pre>" + JSON.stringify(results, null, 2) + "</pre>";
+        },
+        function(error) {
+          console.error("ERROR", error);
+          document.body.innerHTML = "ERROR<pre>" + JSON.stringify(error, null, 2) + "</pre>";
+        }
+      );
+    });
+}
+
+setInterval(function(){ go(); }, 1000);
